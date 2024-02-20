b70ff0d09d2d5299e69a39e5797eb03373d402a2
Daejin <50728381+DevDaejin@users.noreply.github.com>
2024-02-20 17:49:40 +0900

    -아바타 이동 구현 -카메라 회전 구현 -점프 구현 -대시 구현

--- Source/UE_Portfolio/HeroPawn.cpp
+++ Source/UE_Portfolio/HeroPawn.cpp
@@ -2,8 +2,9 @@
 
 
 #include "HeroPawn.h"
-#include "EnhancedInputComponent.h"
-#include "InputmappingContext.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 AHeroPawn::AHeroPawn()
@@ -17,17 +18,57 @@ void AHeroPawn::BeginPlay()
 {
 	Super::BeginPlay();
 
+	SpringArm = FindComponentByClass<USpringArmComponent>();
+	Camera = FindComponentByClass<UCameraComponent>();
+
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+	
+	if (PlayerController)
+	{
+		PlayerController->bShowMouseCursor = false;
+
+		ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer();
+		if (LocalPlayer)
+		{
+			UEnhancedInputLocalPlayerSubsystem* Subsystem =
+				LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+
+			if (Subsystem)
+			{
+				Subsystem->AddMappingContext(CommonMappingContext, 0);
+			}
+			else
+			{
+				UE_LOG(LogTemp, Display, TEXT("Subsystem is nullptr"));
+			}
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("LocalPlayer is nullptr"));
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("PlayerController is nullptr"));
+	}
 }
 
 void AHeroPawn::Jump()
 {
-	if (CurrentJumpCount < MaxJumpCount)
+	if (CurrentJumpCount == 0)
 	{
 		Super::Jump();
-		CurrentJumpCount++;
 	}
-}
+	else if (CurrentJumpCount == 1)
+	{
+		FVector Direction = GetActorUpVector();
+		LaunchCharacter(Direction * SecondJumpForce, false, true);
+	}
+	else
+		return;
 
+	CurrentJumpCount++;
+}
 void AHeroPawn::StopJumping()
 {
 	Super::StopJumping();
@@ -73,16 +114,61 @@ void AHeroPawn::ResetDashCooldown()
 	bCanDash = true;
 }
 
+void AHeroPawn::Move(const FInputActionInstance& Instance)
+{
+	MoveDirection = Instance.GetValue().Get<FVector2D>().GetSafeNormal();
+
+	if (Controller)
+	{
+		FRotator CameraRotator = SpringArm->GetRelativeRotation();
+		CameraRotator.Pitch = 0;
+		CameraRotator.Roll = 0;
+
+		FVector ForwardDirection = FRotationMatrix(CameraRotator).GetUnitAxis(EAxis::X);
+		FVector RightDirection = FRotationMatrix(CameraRotator).GetUnitAxis(EAxis::Y);
+		FVector Direction = ForwardDirection * MoveDirection.Y + RightDirection * MoveDirection.X;
+		if (!Direction.IsNearlyZero())
+		{
+			FRotator CalculatedRotation = FMath::RInterpTo(Controller->GetControlRotation(), Direction.Rotation(), UGameplayStatics::GetWorldDeltaSeconds(this), RotateSpeed);
+			Controller->SetControlRotation(CalculatedRotation);
+		}
+
+		AddMovementInput(Direction.GetSafeNormal(), 1);
+	}
+}
+
+void AHeroPawn::Look(const FInputActionInstance& Instance)
+{
+	FVector2D LookVector = Instance.GetValue().Get<FVector2D>();
+	LookVector.X *= (UGameplayStatics::GetWorldDeltaSeconds(this) * LookingSenstive);
+	LookVector.Y *= (UGameplayStatics::GetWorldDeltaSeconds(this) * LookingSenstive);
+
+	FRotator CalculatedSpringArmRotation = SpringArm->GetComponentRotation();
+	CalculatedSpringArmRotation.Yaw += LookVector.X;
+	CalculatedSpringArmRotation.Pitch -= LookVector.Y;
+	CalculatedSpringArmRotation.Pitch = FMath::Clamp(CalculatedSpringArmRotation.Pitch, MinPitchAngle, MaxPitchAngle);
+	SpringArm->SetWorldRotation(CalculatedSpringArmRotation);
+}
+
 // Called every frame
 void AHeroPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void AHeroPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (Input)
+	{
+		Input->BindAction(IA_Jump, ETriggerEvent::Started, this, &AHeroPawn::Jump);
+		Input->BindAction(IA_Dash, ETriggerEvent::Started, this, &AHeroPawn::Dash);
+		Input->BindAction(IA_Movement, ETriggerEvent::Triggered, this, &AHeroPawn::Move);
+		Input->BindAction(IA_Looking, ETriggerEvent::Triggered, this, &AHeroPawn::Look);
+	}
 }
 
--- Source/UE_Portfolio/HeroPawn.h
+++ Source/UE_Portfolio/HeroPawn.h
@@ -2,7 +2,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "HeroPawn.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
 
 UCLASS()
 class UE_PORTFOLIO_API AHeroPawn : public ACharacter
@@ -13,40 +18,71 @@ public:
 	AHeroPawn();
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+
 	virtual void BeginPlay() override;
 	virtual void Jump() override;
 	virtual void StopJumping() override;
 	virtual void Landed(const FHitResult& Hit) override;
 
-protected:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int32 MaxJumpCount = 2;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int32 MaxDashCount = 2;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float DashSpeed = 6000.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float DashDuriation = 0.2f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float DashCooldown = 1.f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float bCanDash = true;
-private:
-	UPROPERTY(VisibleAnywhere)
-	int32 CurrentJumpCount = 0;
-
-	UPROPERTY(VisibleAnywhere)
-	int32 CurrentDashCount = 0;
-
-	FTimerHandle DashTimerHanlde;
-	FTimerHandle DashCooldownTimerHandle;
-
 	void Dash();
 	void ResetDash();
 	void ResetDashCooldown();
+	void Move(const FInputActionInstance& Instance);
+	void Look(const FInputActionInstance& Instance);
+
+	//Input
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputMappingContext* CommonMappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputAction* IA_Jump;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputAction* IA_Dash;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputAction* IA_Movement;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* IA_Looking;
+
+	//Status
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float MoveAmount = 1000;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float RotateSpeed = 10;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float LookingSenstive = 50;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Status")
+	int32 MaxJumpCount = 2;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Status")
+	int32 MaxDashCount = 2;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Status")
+	float SecondJumpForce = 600.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Status")
+	float DashSpeed = 600.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Status")
+	float DashDuriation = 0.2f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Status")
+	float DashCooldown = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Status")
+	bool bCanDash = true;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Status")
+	int32 CurrentJumpCount = 0;
+
+	UPROPERTY(VisibleAnywhere, Category = "Status")
+	int32 CurrentDashCount = 0;
+
+	FVector2D MoveDirection;
+	FTimerHandle DashTimerHanlde;
+	FTimerHandle DashCooldownTimerHandle;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	float MinPitchAngle = -85;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	float MaxPitchAngle = 85;
+
+	USpringArmComponent* SpringArm;
+	UCameraComponent* Camera;
 };
